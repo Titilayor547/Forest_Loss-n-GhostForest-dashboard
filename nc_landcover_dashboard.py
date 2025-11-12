3cd1f3f1c45472ce52cbf54745919b0a9ded6f76
Titilayo Tajudeen <tttajude@ncsu.edu>
2025-11-12 12:01:00 -0500

    updated link

--- nc_landcover_dashboard.py
+++ nc_landcover_dashboard.py
@@ -64,8 +64,21 @@ raster_color_map = list(raster_color_map.values())
 # ---------------------------
 # LOAD RASTERS
 # ---------------------------
-path_1993 = "https://raw.githubusercontent.com/Titilayor547/Forest_Loss-n-GhostForest-dashboard/main/pred_Land1985a.tif"
-path_2000 = "https://raw.githubusercontent.com/Titilayor547/Forest_Loss-n-GhostForest-dashboard/main/pred_Land2021a.tif"
+#path_1993 = "c:\\Users\\tttajude\\PHDresearch\\GhostForest\\Processed_Image\\pred_Land1985a.tif"
+#path_2000 = "c:\\Users\\tttajude\\PHDresearch\\GhostForest\\Processed_Image\\pred_Land2021check.tif"
+
+def download_tif(url, filename):
+    r = requests.get(url, stream=True)
+    r.raise_for_status()
+    with open(filename, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+# Download the rasters first
+download_tif("https://raw.githubusercontent.com/Titilayor547/Forest_Loss-n-GhostForest-dashboard/main/pred_Land1985a.tif",
+             "pred_Land1985a.tif")
+download_tif("https://raw.githubusercontent.com/Titilayor547/Forest_Loss-n-GhostForest-dashboard/main/pred_Land2021a.tif",
+             "pred_Land2021a.tif")
 
 df_1993 = pd.DataFrame({'Landcover': [], 'Area_Pixels': []})
 df_2000 = pd.DataFrame({'Landcover': [], 'Area_Pixels': []})
@@ -75,7 +88,7 @@ array_2000 = None
 
 try:
     # --- Load 1993 Data ---
-    with rasterio.open(path_1993) as src:
+    with rasterio.open("pred_Land1985a.tif") as src:
         array_1993 = src.read(1)
         values_1993, counts_1993 = np.unique(array_1993, return_counts=True)
         valid_indices_1993 = values_1993 != -1
@@ -86,7 +99,7 @@ try:
         df_1993['Landcover'] = df_1993['Class_ID'].map(classes)
 
     # --- Load 2000 Data ---
-    with rasterio.open(path_2000) as src:
+    with rasterio.open("pred_Land2021a.tif") as src:
         array_2000 = src.read(1)
         values_2000, counts_2000 = np.unique(array_2000, return_counts=True)
         valid_indices_2000 = values_2000 != -1
